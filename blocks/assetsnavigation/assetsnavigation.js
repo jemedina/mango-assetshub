d796983eb09c899a4943e7b939a056346cb7487b
Eduardo <eduardo.medina@anseris.es>
2026-07-08 08:21:12 -0600

    Add assets navigation base block

--- blocks/assetsnavigation/assetsnavigation.js
+++ blocks/assetsnavigation/assetsnavigation.js
@@ -3,14 +3,8 @@
  * @param {Element} block The footer block element
  */
 export default async function decorate(block) {
-  // decorate footer DOM
   block.textContent = '';
-  const wrapper = document.createElement('div');
-  const body = document.createElement('h2');
-
-  body.innerHTML = 'Hi, Im the assets listing block';
-
-  wrapper.append(body);
-
-  block.append(wrapper);
+  const heading = document.createElement('h2');
+  heading.textContent = "Hi, I'm the assets listing block";
+  block.append(heading);
 }
